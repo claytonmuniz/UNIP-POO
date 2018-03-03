88078190c7889c1c068bfcd023068a7edf461fbf
claytonmuniz <36775492+claytonmuniz@users.noreply.github.com>
2018-03-02 21:35:03 -0300

    att do programa

---
+++ aula-1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using aula_1.Modelo;
+namespace aula_1
+{
+    public partial class frmPrincipal : Form
+    {
+        Controle controle = new Controle();
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+                       lblResultado.Text = controle.calcular(txbPrimeiroNumero.Text, txbSegundoNumero.Text, "+");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            lblResultado.Text = controle.calcular(txbPrimeiroNumero.Text, txbSegundoNumero.Text, "-");
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            
+            lblResultado.Text = controle.calcular(txbPrimeiroNumero.Text, txbSegundoNumero.Text, "/");
+        }
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            
+            lblResultado.Text = controle.calcular(txbPrimeiroNumero.Text, txbSegundoNumero.Text, "*");
+        }
+    }
+}
---
+++ aula-1/Modelo/Controle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_1.Modelo
+{
+    public class Controle
+    {
+        public String calcular(String num1, String num2, String op)
+        {
+            Double n1 = 0.0, n2 = 0.0, resultado = 0.0; //criando variaveis
+            String resposta = "";
+            try
+            {
+                n1 = Convert.ToDouble(num1);
+                n2 = Convert.ToDouble(num2);
+                if (op.Equals("+"))
+                    resultado = n1 + n2;
+                if (op.Equals("-"))
+                    resultado = n1 - n2;
+                if (op.Equals("*"))
+                    resultado = n1 * n2;
+                resposta = resultado.ToString("0.##");
+                if (op.Equals("/"))
+                    if (n2 == 0)
+                    {
+                        resposta = "Nâo é possivel dividir por 0";
+                        num1 = "";
+                        num2 = "";
+                    }
+                    else
+                    {
+                        resultado = n1 / n2;
+                        resposta = resultado.ToString("0.##");
+                    }
+
+            }
+            catch (FormatException e)
+            {
+                resposta = "Digite um número valido doente";
+                num1 = "";
+                num2 = "";
+            }
+            return resposta;
+
+
+
+        }
+    }
+}
